69f939281b8f12886e808036dd54348930a08d5b
wistic <prathameshkatkar11@gmail.com>
2020-11-26 14:26:36 +0530

    Added hmm

--- library/dirwalk.py
+++ library/dirwalk.py
@@ -1,16 +1,13 @@
 import os
 
 
-def dirWalk(source_path, output_path, file_list):
+def dirWalk(source_path, file_list):
     listing = os.listdir(source_path)
     for entry in listing:
-        absolute_path = source_path + entry
+        absolute_path = os.path.join(source_path, entry)
         if os.path.isdir(absolute_path):
-            if absolute_path[-1] != '/':
-                absolute_path = absolute_path + '/'
-            dirWalk(absolute_path, output_path, file_list)
+            dirWalk(absolute_path, file_list)
         else:
             if absolute_path.split('.')[-1] == 'xml':
-                file_entry = (source_path, entry, output_path)
-                file_list.append(file_entry)
+                file_list.append(absolute_path)
     return file_list
